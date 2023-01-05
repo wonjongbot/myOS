2e0dde4b08571e8b70afbb23c3056afbbc705ebf
wonjongbot <13744405+wonjongbot@users.noreply.github.com>
2023-01-05 15:38:15 +0900

    update to clear the screen

--- boot/main.c
+++ boot/main.c
@@ -4,7 +4,7 @@ int start(){
     vram = (char*) 0xb8000;
 
     for(int i = 0; i < 2 * 25 * 80; i +=2){
-        *(vram + i) = 0x01;
+        *(vram + i) = 0x00;
         *(vram + i + 1)  = 0x1c;
     }
     return 0;
